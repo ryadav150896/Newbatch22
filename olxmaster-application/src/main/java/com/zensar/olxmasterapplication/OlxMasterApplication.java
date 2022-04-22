70a4bc2d8399b974e4134d9e2b7243ceea33a7ed
ryadav150896 <r.yadav1@zensar>
2022-04-22 16:31:02 +0530

    olxmaster application

---
+++ olxmaster-application/src/main/java/com/zensar/olxmasterapplication/OlxMasterApplication.java
@@ -0,0 +1,13 @@
+package com.zensar.olxmasterapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlxMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlxMasterApplication.class, args);
+	}
+
+}
---
+++ olxmaster-application/src/main/java/com/zensar/olxmasterapplication/entity/OlxMasterStatus.java
@@ -0,0 +1,14 @@
+package com.zensar.olxmasterapplication.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OlxMasterStatus {
+	private int id;
+	private String Status;
+
+}
