2959cdd71381272a28e9c7f8afacb0d276ef87b6
ryadav150896 <102963614+ryadav150896@users.noreply.github.com>
2022-05-07 00:58:09 +0530

    Delete olx-application directory

--- olx-application/src/main/java/com/zensar/olxapplication/service/UserLoginService.java
+++
@@ -1,9 +0,0 @@
-package com.zensar.olxapplication.service;
-
-import com.zensar.olxapplication.entity.UserLogin;
-
-public interface UserLoginService {
-	
-	String loginUser(UserLogin userLogin, String token) ;
-
-}
